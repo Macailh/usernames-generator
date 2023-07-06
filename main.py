147bb016d7198d5ced61c7b1ab06941543f9a038
Macailh <salvadorenriquegb@gmail.com>
2023-07-05 19:43:53 -0600

    refactor: to generate more understandable usernames

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import random
 import string
 
 
-def generate_username(longitud):
-    characters = string.ascii_letters + string.digits
-    username = ''.join(random.choice(characters)
-                       for _ in range(longitud))
+def generate_username(length):
+    consonants = ''.join(random.choice(string.ascii_lowercase)
+                         for _ in range(length // 2))
+    vowels = ''.join(random.choice('aeiou')
+                     for _ in range(length - (length // 2)))
+    username = ''.join(random.choice(consonants + vowels)
+                       for _ in range(length))
     return username
 
 
 username_length = 8
-username = generate_username(username_length)
-print("Generated username:", username)
+generated_username = generate_username(username_length)
+print("Generated username:", generated_username)
